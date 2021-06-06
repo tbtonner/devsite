ee9ae273b1a7d7e3b8ccdb9fadfa367e034dad23
tbtonner <agcdoctor@gmail.com>
2021-06-06 18:53:04 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'tbtonner.dev' });
+    res.render('index', { title: 'tbtonner' });
 });
 
 module.exports = router;
